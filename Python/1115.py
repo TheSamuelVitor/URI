9120852da26308eb985633d782e66e760566849f
TheSamuelVitor <thesamuelvitor.py@gmail.com>
2022-06-25 12:52:35 -0300

    add python problems

---
+++ Python/1115.py
@@ -0,0 +1,7 @@
+x,y = input().split('')
+x = int(x)
+y = int(x)
+if x > 0 and y>0:
+    print('primeiro')
+elif x < 0 and y > 0:
+    print('segundo')
---
+++ Python/2862.py
@@ -0,0 +1,7 @@
+qtd = int(input())
+for i in range (qtd):
+    n = int(input())
+    if n > 8000:
+        print('Mais de 8000!')
+    else:
+        print('Inseto!')
