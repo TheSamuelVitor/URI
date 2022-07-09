898c2a1452d1f2b03985d324f0218e1840c16a05
TheSamuelVitor <thesamuelvitor.py@gmail.com>
2022-07-09 11:20:28 -0300

    feat: 1007 solution

---
+++ JavaScript/1007/script.js
@@ -0,0 +1,12 @@
+// Leia quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).
+var input = require('fs').readFileSync('stdin', 'utf8')
+var lines = input.split('\n')
+
+var a = parseInt(lines.shift())
+var b = parseInt(lines.shift())
+var c = parseInt(lines.shift())
+var d = parseInt(lines.shift())
+
+var diferenca = (a*b - c*d)
+
+console.log(`DIFERENCA = ${diferenca}`)
