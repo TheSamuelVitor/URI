e21309a6519c9a3b5255dfbf62a973c35da92927
TheSamuelVitor <thesamuelvitor.py@gmail.com>
2022-11-02 23:12:36 -0300

    :bug: fix: fix test bug

--- Go/problemas/1037 - intervalo_test.go
+++ Go/problemas/1037 - intervalo_test.go
@@ -53,7 +53,17 @@ func TestIntervalo(t *testing.T) {
 		{ name: "test 25 a 50", got: Intervalo(26.00), want: "Intervalo [0,25]"},
 		{ name: "test 50 a 75", got: Intervalo(55.00), want: "Intervalo [0,25]"},
 		{ name: "test 75 a 100", got: Intervalo(25.00), want: "Intervalo [0,25]"},
-		{ name: "test fora de intervalo", got: Intervalo(25.00), want: "Fora de intervalo"},
+		{ name: "test fora de intervalo", got: Intervalo(25.00), want: "Fora de intervalo"}
+	}
+
+	for _, test := range intervaloTests {
+		t.Run(test.name, func(t *testing.T) {
+			got := test.got
+			want := test.want
+			if got != want {
+				t.Errorf("\ngot %v\nwant %v", got, want)
+			}
+		}
 	}
 
 }
