ec862556c5dadba602ecdcdbf073b2574286789d
thesamuelvitor <thesamuelvitor.py@gmail.com>
2023-05-29 14:05:30 -0300

    :sparkles: feat: adicionando resolucao do problema 1048

--- Go/main.go
+++ Go/main.go
@@ -8,5 +8,8 @@ import (
 func main() {
 	var salario float64
 	fmt.Scanf("%f", &salario)
-	problemas.AumentoDeSalario(salario)
+	var reajuste, porcentagem = problemas.AumentoDeSalario(salario)
+	fmt.Printf("Novo salario: %.2f\n", salario+reajuste)
+	fmt.Printf("Reajuste ganho: %.2f\n", reajuste)
+	fmt.Printf("Em percentual: %s\n", porcentagem)
 }
--- Go/problemas/1048 - aumentodesalario.go
+++ Go/problemas/1048 - aumentodesalario.go
@@ -1,33 +1,23 @@
 package problemas
 
-import "fmt"
-
 // funcao que recebe o valor de salario e retorna o salario com a modificacao
 func AumentoDeSalario(salario float64) (reajuste float64, percentual string) {
-	if (salario >= 0 && salario <= 400) {
+	if salario >= 0 && salario <= 400 {
 		reajuste = salario * 0.15
-		salario = salario + reajuste
 		percentual = "15%"
-	} else if (salario >= 400.01 && salario <= 800) {
+	} else if salario >= 400.01 && salario <= 800 {
 		reajuste = salario * 0.12
-		salario = salario + reajuste
 		percentual = "12%"
-	}	else if (salario >= 800.01 && salario <= 1200) {
+	} else if salario >= 800.01 && salario <= 1200 {
 		reajuste = salario * 0.1
-		salario = salario + reajuste
 		percentual = "10%"
-	}	else if (salario >= 1200.01 && salario <= 2000) {
+	} else if salario >= 1200.01 && salario <= 2000 {
 		reajuste = salario * 0.07
-		salario = salario + reajuste
 		percentual = "7%"
-	}	else if (salario > 2000) {
-		reajuste = salario * 0.05
-		salario = salario + reajuste
-		percentual = "5%"
+	} else if salario > 2000 {
+		reajuste = salario * 0.04
+		percentual = "4%"
 	}
-	
-	fmt.Println("Novo salario: ", salario)
-	fmt.Println("Reajuste ganho: ", reajuste)
-	fmt.Println("Em percentual: ", percentual)
-	return salario, reajuste, percentual
+
+	return reajuste, percentual
 }
