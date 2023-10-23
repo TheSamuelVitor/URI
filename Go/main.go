e24b6f10789a9de0924a8054156cc1c4cda38760
TheSamuelVitor <thesamuelvitor.py@gmail.com>
2023-10-23 11:34:59 -0300

    feat: adicionando resolucao

--- Go/main.go
+++ Go/main.go
@@ -6,10 +6,7 @@ import (
 )
 
 func main() {
-	var salario float64
-	fmt.Scanf("%f", &salario)
-	var reajuste, porcentagem = problemas.AumentoDeSalario(salario)
-	fmt.Printf("Novo salario: %.2f\n", salario+reajuste)
-	fmt.Printf("Reajuste ganho: %.2f\n", reajuste)
-	fmt.Printf("Em percentual: %s\n", porcentagem)
+	var a, b, c string
+	fmt.Scanf("%s\n%s\n%s", &a, &b, &c)
+	fmt.Println(problemas.Animal(a, b, c))
 }
